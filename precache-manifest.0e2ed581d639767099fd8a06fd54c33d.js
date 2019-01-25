48751e2f0b6357f37ba770ab204efd75a308ba02
David Andrews <contactdardino@gmail.com>
2019-01-25 17:20:32 +0000

    Updates

---
+++ precache-manifest.0e2ed581d639767099fd8a06fd54c33d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3b2d7b484a6bf66d6fa",
+    "url": "/simpsons-quote-generator/static/js/runtime~main.e3b2d7b4.js"
+  },
+  {
+    "revision": "4ff7a71f176466c5f19b",
+    "url": "/simpsons-quote-generator/static/js/main.4ff7a71f.chunk.js"
+  },
+  {
+    "revision": "d5dbbc275c004943d614",
+    "url": "/simpsons-quote-generator/static/js/1.d5dbbc27.chunk.js"
+  },
+  {
+    "revision": "4ff7a71f176466c5f19b",
+    "url": "/simpsons-quote-generator/static/css/main.5129932b.chunk.css"
+  },
+  {
+    "revision": "f7ede6e671eabb45cad0ca91fc8e40ce",
+    "url": "/simpsons-quote-generator/index.html"
+  }
+];
